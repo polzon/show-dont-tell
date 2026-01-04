d1cfbadccd42ef57a3fea272076e8eb7a83c3956
Zack Polson <zack@polson.dev>
2026-01-04 01:20:51 -0700

    fixed small glitch with the behavior editor not properly disabling

--- editor/behavior_graph.gd
+++ editor/behavior_graph.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 const EDITOR_TSCN_UID = "uid://6n7e8d62o8a6"
 const DOCK_SETTING_NAME = "addons/show_not_tell/enable_dock"
-const DOCK_SETTING_DEFAULT = true
+const DOCK_SETTING_DEFAULT = false
 
 
 static func is_dock_enabled() -> bool:
--- plugin/plugin.gd
+++ plugin/plugin.gd
@@ -3,40 +3,49 @@ class_name ShowNotTellPlugin
 extends EditorPlugin
 
 const EDITOR_CONTROL_NAME = "Behavior Graph"
+const EDITOR_TSCN = preload(BehaviorGraph.EDITOR_TSCN_UID)
 
-static var _dock_setting_enabled: bool = false
-static var _dock_was_added: bool = false
+static var _instance: ShowNotTellPlugin
 
 ## The [PackedScene] of the editor node that is added to the Editor.
 var gridmap_dock: Control
+var _dock_was_enabled: bool = false
+
+@onready var editor_enabled: bool = true:
+	set = set_editor_enabled
 
 
-func _ready() -> void:
+func _init() -> void:
+	_instance = self
+	ProjectSettings.settings_changed.connect(_on_project_settings_changed)
 	_load_project_settings()
-	const EDITOR_TSCN = preload(BehaviorGraph.EDITOR_TSCN_UID)
-	if EDITOR_TSCN and EDITOR_TSCN.can_instantiate():
-		gridmap_dock = EDITOR_TSCN.instantiate()
-		_setup_dock()
 
 
-func _setup_dock() -> void:
-	if BehaviorGraph.is_dock_enabled():
+func _disable_plugin() -> void:
+	_remove_dock()
+
+
+func set_editor_enabled(enabled: bool) -> void:
+	editor_enabled = enabled
+	if editor_enabled:
 		_create_dock()
+	else:
+		_remove_dock()
 
 
 func _create_dock() -> void:
-	assert(not _dock_was_added, "Trying to create dock after it was created!")
-	if not _dock_was_added:
+	if not _dock_was_enabled:
+		if not gridmap_dock and EDITOR_TSCN and EDITOR_TSCN.can_instantiate():
+			gridmap_dock = EDITOR_TSCN.instantiate()
 		add_control_to_bottom_panel(gridmap_dock, EDITOR_CONTROL_NAME)
-		_dock_was_added = true
+		_dock_was_enabled = true
 
 
 func _remove_dock() -> void:
-	assert(_dock_was_added, "Dock trying to be removed before it was added!")
-	if _dock_was_added:
+	if _dock_was_enabled and gridmap_dock:
 		remove_control_from_bottom_panel(gridmap_dock)
 		gridmap_dock.queue_free()
-		_dock_was_added = false
+		_dock_was_enabled = false
 
 
 func _load_project_settings() -> void:
@@ -46,12 +55,7 @@ func _load_project_settings() -> void:
 	if not ProjectSettings.has_setting(NAME):
 		ProjectSettings.set_setting(NAME, DEFAULT)
 	ProjectSettings.set_initial_value(NAME, DEFAULT)
-	ProjectSettings.settings_changed.connect(_on_project_settings_changed)
 
 
 func _on_project_settings_changed() -> void:
-	_dock_setting_enabled = BehaviorGraph.is_dock_enabled()
-	if not _dock_setting_enabled and _dock_was_added:
-		_remove_dock()
-	elif _dock_setting_enabled and not _dock_was_added:
-		_create_dock()
+	editor_enabled = BehaviorGraph.is_dock_enabled()
