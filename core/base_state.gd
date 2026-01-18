49d21da8d41f65fedf8de8490acdc7544ac6aa87
Zack Polson <zack@polson.dev>
2026-01-17 21:33:29 -0700

    initial work on auto toggling of processing modes

--- core/base_state.gd
+++ core/base_state.gd
@@ -2,12 +2,68 @@ class_name BaseState
 extends Node
 ## The origin node that all behavior derrives from.
 
+## Emitted when the [BaseState] has been entered/started.
+signal started
+## Emitted when the [BaseState] has been exited/stopped.
+signal ended
+
+## If set to `true`, the processing will be automatically toggled off or on
+## when the state is changed. It will automatically toggle [method _process]
+## and [method _physics_process] if they are overwritten and enabled
+## in the node file. By default, both are `false`.
+var process_on_active: bool = _is_any_processing_enabled():
+	set = set_process_on_active
+
+
+func set_process_on_active(is_enabled: bool) -> void:
+	process_on_active = is_enabled
+	_setup_physics_signal(process_on_active)
+	_setup_process_signal(process_on_active)
+
 
 ## Processed when the [BaseState] has been entered.
 func _entered_state() -> void:
-	pass
+	started.emit()
 
 
 ## Processed when the [BaseState] has exited.
 func _exited_state() -> void:
-	pass
+	ended.emit()
+
+
+func _setup_process_signal(is_enabled: bool) -> void:
+	if has_method("_process"):
+		if is_enabled:
+			if not started.is_connected(set_process.bind(true)):
+				started.connect(set_process.bind(true))
+			if not ended.is_connected(set_process.bind(false)):
+				ended.connect(set_process.bind(false))
+		else:
+			if started.is_connected(set_process.bind(true)):
+				started.disconnect(set_process.bind(true))
+			if ended.is_connected(set_process.bind(false)):
+				ended.disconnect(set_process.bind(false))
+
+
+func _setup_physics_signal(is_enabled: bool) -> void:
+	if has_method("_physics_process"):
+		if is_enabled:
+			if not started.is_connected(set_physics_process.bind(true)):
+				started.connect(set_physics_process.bind(true))
+			if not ended.is_connected(set_physics_process.bind(false)):
+				ended.connect(set_physics_process.bind(false))
+		else:
+			if started.is_connected(set_physics_process.bind(true)):
+				started.disconnect(set_physics_process.bind(true))
+			if ended.is_connected(set_physics_process.bind(false)):
+				ended.disconnect(set_physics_process.bind(false))
+
+
+## Returns if *either* [method _process] or [method _physics_processing] are
+## overwritten and enabled in the script.
+func _is_any_processing_enabled() -> bool:
+	var processing_enabled := \
+			has_method("_process") \
+			or has_method("_physics_process")
+	set_process_on_active(processing_enabled)
+	return processing_enabled
