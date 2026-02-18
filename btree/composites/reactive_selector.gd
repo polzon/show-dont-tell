6dc5130241ed18a7faf95958c30de3c20b1a4459
Zack Polson <zack@polson.dev>
2026-02-17 18:24:34 -0700

    some documentation
    Signed-off-by: Zack Polson <zack@polson.dev>

--- btree/composites/reactive_selector.gd
+++ btree/composites/reactive_selector.gd
@@ -1,6 +1,11 @@
 @icon("res://addons/show_not_tell/icons/selector_reactive.svg")
 class_name BT_ReactiveSelector
 extends BT_SelectorComposite
+## Reactive selector composite.
+##
+## Ticks children from left to right every frame, always starting at the first
+## child. Returns SUCCESS on the first child success, RUNNING on the first child
+## still running, and FAILED only if all children fail.
 
 
 func _tick(delta: float) -> Status:
--- btree/composites/reactive_sequence.gd
+++ btree/composites/reactive_sequence.gd
@@ -1,6 +1,13 @@
 @icon("res://addons/show_not_tell/icons/sequence_reactive.svg")
 class_name BT_ReactiveSequence
 extends BT_SequenceComposite
+## Reactive sequence composite.
+##
+## Ticks children from left to right every frame, always starting at the first
+## child. Returns FAILED on the first child failure, RUNNING on the first child
+## still running, and SUCCESS only if all children succeed.
+##
+## @tutorial(Beehave Reference): https://bitbra.in/beehave/#/manual/selector
 
 
 func _tick(delta: float) -> Status:
