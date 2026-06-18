4faf7b1116d31b2e9bffbbf8ed733c3be5a5715d
Zack Polson <zack@polson.dev>
2026-06-18 00:27:17 -0600

    testing some different methods
    Signed-off-by: Zack Polson <zack@polson.dev>

---
+++ addons/show_dont_tell/src/fsm/transitions/transition_on_actions.gd
@@ -0,0 +1,4 @@
+class_name TransitionOnActions
+extends TransitionExample
+
+@export var trigger_action: Array[TransitionSubrule] = []
---
+++ addons/show_dont_tell/src/fsm/transitions/transition_subrule.gd
@@ -0,0 +1,4 @@
+class_name TransitionSubrule
+extends Resource
+
+@export_custom(PROPERTY_HINT_INPUT_NAME, "") var trigger_action: StringName
