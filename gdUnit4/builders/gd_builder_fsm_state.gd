c3be08b71a7227ba2673c38af60790f78bc6fcd9
Zack Polson <zack@polson.dev>
2026-06-20 18:15:23 -0600

    add creation of conditions
    Signed-off-by: Zack Polson <zack@polson.dev>

--- gdUnit4/builders/gd_builder_fsm_state.gd
+++ gdUnit4/builders/gd_builder_fsm_state.gd
@@ -9,6 +9,10 @@ func _notification(what: int) -> void:
 		_root.free()
 
 
+func build() -> FiniteState:
+	return _root
+
+
 func create_state(data_script: GDScript = null) -> GdBuilderFsmState:
 	_root = FiniteState.new()
 	if data_script:
@@ -28,5 +32,30 @@ func _set_state_data(state: FiniteState, data_script: GDScript) -> void:
 			data_obj.free()
 
 
-func build() -> FiniteState:
-	return _root
+func create_condition(condition_script: GDScript) -> GdBuilderFsmState:
+	if (
+		not condition_script
+		or not condition_script.can_instantiate()
+		or not _root
+	):
+		return self
+
+	var condition_node := TransitionCondition.new()
+	if condition_script:
+		_set_condition_data(condition_node, condition_script)
+	_root.add_child(condition_node)
+	return self
+
+
+func _set_condition_data(
+	condition: TransitionCondition, data_script: GDScript
+) -> void:
+	if not data_script or not data_script.can_instantiate():
+		return
+
+	if condition:
+		var data_obj: Variant = data_script.new()
+		if data_obj is TransitionOnCondition:
+			condition.condition = data_obj
+		elif data_obj is not RefCounted:
+			data_obj.free()
