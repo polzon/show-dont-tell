36d6605c8e6d98f99eb7a564fd8d79ca1f17c4de
Zack Polson <zack@polson.dev>
2026-06-26 16:11:35 -0700

    expanded on fsm state builder and added new tests
    Signed-off-by: Zack Polson <zack@polson.dev>

--- gdUnit4/builders/gd_builder_fsm_state.gd
+++ gdUnit4/builders/gd_builder_fsm_state.gd
@@ -2,6 +2,8 @@ class_name GdBuilderFsmState
 extends RefCounted
 
 var _root: FiniteState
+var _nodes: Array[Node] = []:
+	get = get_all_nodes
 
 
 func _notification(what: int) -> void:
@@ -9,30 +11,37 @@ func _notification(what: int) -> void:
 		_root.free()
 
 
-func build() -> FiniteState:
+func get_root() -> FiniteState:
 	return _root
 
 
-func create_state(data_script: GDScript = null) -> GdBuilderFsmState:
-	_root = FiniteState.new()
+func get_all_nodes() -> Array[Node]:
+	return _nodes
+
+
+static func new_state(data_script: GDScript = null) -> GdBuilderFsmState:
+	var new_builder := GdBuilderFsmState.new()
+	var finite_state := FiniteState.new()
 	if data_script:
-		_set_state_data(_root, data_script)
-	return self
+		new_builder._set_state_data(finite_state, data_script)
+	new_builder._root = finite_state
+	new_builder._nodes = [finite_state]
+	return new_builder
 
 
-func _set_state_data(state: FiniteState, data_script: GDScript) -> void:
+func _set_state_data(state_node: FiniteState, data_script: GDScript) -> void:
 	if not data_script or not data_script.can_instantiate():
 		return
 
-	if state:
+	if state_node:
 		var data_obj: Variant = data_script.new()
 		if data_obj is StateData:
-			state.state_data = data_obj
+			state_node.state_data = data_obj
 		elif data_obj is not RefCounted:
 			data_obj.free()
 
 
-func create_condition(condition_script: GDScript) -> GdBuilderFsmState:
+func if_condition(condition_script: GDScript) -> GdBuilderFsmState:
 	if (
 		not condition_script
 		or not condition_script.can_instantiate()
@@ -43,26 +52,64 @@ func create_condition(condition_script: GDScript) -> GdBuilderFsmState:
 	var condition_node := TransitionCondition.new()
 	if condition_script:
 		_set_condition_data(condition_node, condition_script)
-	var deepest_node := _get_deepest_node(_root)
-	deepest_node.add_child(condition_node)
+	var last_node := _get_last_node()
+	_add_node(last_node, condition_node)
 	return self
 
 
 func _set_condition_data(
-	condition: TransitionCondition, data_script: GDScript
+	condition_node: TransitionCondition, data_script: GDScript
 ) -> void:
 	if not data_script or not data_script.can_instantiate():
 		return
 
-	if condition:
+	if condition_node:
 		var data_obj: Variant = data_script.new()
 		if data_obj is TransitionOnCondition:
-			condition.condition = data_obj
+			condition_node.condition = data_obj
 		elif data_obj is not RefCounted:
 			data_obj.free()
 
 
-func _get_deepest_node(node: Node) -> Node:
-	if node.get_child_count() == 0:
-		return node
-	return _get_deepest_node(node.get_child(0))
+func exit_to(exit_node: Node) -> GdBuilderFsmState:
+	var last_condition_node := _get_lastest_condition_node()
+	assert(last_condition_node, "Failed to find condition node.")
+	if not last_condition_node:
+		return self
+
+	assert(exit_node, "Exit node is invalid.")
+	if last_condition_node and exit_node:
+		var new_exit := TransitionExit.new()
+		new_exit.exit_node = exit_node
+		_add_node(last_condition_node, new_exit)
+	return self
+
+
+func _get_last_node() -> Node:
+	if _nodes.is_empty():
+		return null
+	return _nodes[_nodes.size() - 1]
+
+
+## Returns a duplicated array of [member _nodes] in reverse order, so we can
+## find the last inserted node.
+func _get_reverse_nodes() -> Array[Node]:
+	var reverse_arr := _nodes.duplicate()
+	reverse_arr.reverse()
+	return reverse_arr
+
+
+func _get_lastest_condition_node() -> TransitionCondition:
+	for node: Node in _get_reverse_nodes():
+		if node is TransitionCondition:
+			return node
+	push_error("Failed to find a TransitionCondition node!")
+	return null
+
+
+func _add_node(parent: Node, child: Node = null) -> void:
+	assert(_nodes.has(parent), "Parent isn't added in the array!")
+	assert(not _nodes.has(child))
+	if child:
+		parent.add_child(child)
+		_nodes.push_back(child)
--- gdUnit4/unit/fsm/test_builder_fsm_state.gd
+++ gdUnit4/unit/fsm/test_builder_fsm_state.gd
@@ -1,10 +1,14 @@
 extends GdUnitTestSuite
+## Unit tests for [GdBuilderFsmState].
+## [br]
+## Link: 'res://addons/show_dont_tell/gdUnit4/builders/gd_builder_fsm_state.gd'
 
 
 func test_state_creation_valid() -> void:
-	var builder := GdBuilderFsmState.new()
-	builder.create_state(StateDataMove)
-	var root := builder.build()
+	var builder := GdBuilderFsmState.new_state(StateDataMove)
+	assert_object(builder).is_not_null()
+
+	var root := builder.get_root()
 	add_child(root)
 
 	assert_object(root).is_not_null()
@@ -14,10 +18,9 @@ func test_state_creation_valid() -> void:
 func test_state_creation_invalid(
 	wrong_type: Variant, _test_parameters := [[null], [FiniteState]]
 ) -> void:
-	var builder := GdBuilderFsmState.new()
 	var wrong_script: GDScript = wrong_type
-	builder.create_state(wrong_script)
-	var root := builder.build()
+	var builder := GdBuilderFsmState.new_state(wrong_script)
+	var root := builder.get_root()
 	add_child(root)
 
 	assert_object(root).is_not_null()
@@ -25,10 +28,10 @@ func test_state_creation_invalid(
 
 
 func test_add_condition_valid() -> void:
-	var builder := GdBuilderFsmState.new()
-	builder.create_state(StateDataMove)
-	builder.create_condition(TransitionOnCommand)
-	var root := builder.build()
+	var builder := GdBuilderFsmState.new_state(StateDataMove).if_condition(
+		TransitionOnCommand
+	)
+	var root := builder.get_root()
 	add_child(root)
 
 	assert_object(root).is_not_null()
@@ -36,3 +39,54 @@ func test_add_condition_valid() -> void:
 	var condition_node := root.get_child(0) as TransitionCondition
 	assert_object(condition_node).is_not_null()
 	assert_object(condition_node.condition).is_not_null()
+
+
+func test_create_condition_exit() -> void:
+	var first_builder := GdBuilderFsmState.new_state(StateDataIdle)
+	add_child(first_builder.get_root())
+
+	var second_builder := (
+		GdBuilderFsmState
+		. new_state(StateDataIdle)
+		. if_condition(TransitionOnCommand)
+		. exit_to(first_builder.get_root())
+	)
+	add_child(second_builder.get_root())
+
+	var nodes := second_builder.get_all_nodes()
+	assert_array(nodes).is_not_empty()
+	assert_array(nodes).has_size(3)
+
+	var state_count := _state_count(nodes)
+	var condition_count := _condition_count(nodes)
+	var exit_count := _exit_count(nodes)
+	(
+		assert_int(state_count)
+		. is_equal(condition_count)
+		. is_equal(exit_count)
+		. is_equal(1)
+	)
+
+
+func _state_count(data_arr: Array) -> int:
+	return (
+		data_arr
+		. filter(func(v: Variant) -> bool: return v is FiniteState)
+		. size()
+	)
+
+
+func _condition_count(data_arr: Array) -> int:
+	return (
+		data_arr
+		. filter(func(v: Variant) -> bool: return v is TransitionCondition)
+		. size()
+	)
+
+
+func _exit_count(data_arr: Array) -> int:
+	return (
+		data_arr
+		. filter(func(v: Variant) -> bool: return v is TransitionExit)
+		. size()
+	)
