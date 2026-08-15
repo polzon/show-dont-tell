a936468cd5867743b1e5b2d3a4b7afcd5593d15e
Zack Polson <zack@polson.dev>
2026-08-14 21:30:18 -0600

    more refactor and cleanup
    Signed-off-by: Zack Polson <zack@polson.dev>

--- addons/show_dont_tell/src/fsm/transitions/conditions/transition_on_command.gd
+++ addons/show_dont_tell/src/fsm/transitions/conditions/transition_on_command.gd
@@ -3,12 +3,11 @@ class_name TransitionOnCommand
 extends TransitionOnCondition
 ## Passes a transition check when an expected [Command] is passed to this.
 
-# TODO: Move this _get _set command_list logic to a reusable util.
-
-const COMMAND_PROPERTY_NAME: StringName = "command_type"
+const _COMMAND_PROPERTY: StringName = &"command_script"
 
 @export_group("Debug")
 @export var enable_debug: bool = false
+@export_group("")
 
 var _command_script: GDScript
 var _matched_command: bool = false
@@ -38,33 +37,15 @@ func _can_transition() -> bool:
 	return true
 
 
-func _configuration_warning() -> PackedStringArray:
-	var warnings: PackedStringArray = []
-	if not _command_script:
-		warnings.append("Command type is empty.")
-	elif not _is_command_script(_command_script):
-		warnings.append("Command type must extend Command.")
-	return warnings
-
-
-func _is_command_script(script: GDScript) -> bool:
-	var current: Script = script
-	while current:
-		if current == Command:
-			return true
-		current = current.get_base_script()
-	return false
-
-
 func _get_friendly_name() -> String:
 	return "CommandIsReceived"
 
 
 func _get_property_list() -> Array[Dictionary]:
-	var class_names := _collect_command_class_names()
+	var class_names := SdtUtils.get_class_list_of_type(Command)
 	return [
 		{
-			"name": COMMAND_PROPERTY_NAME,
+			"name": _COMMAND_PROPERTY,
 			"type": TYPE_STRING_NAME,
 			"hint": PROPERTY_HINT_ENUM,
 			"hint_string": ",".join(class_names),
@@ -74,40 +55,15 @@ func _get_property_list() -> Array[Dictionary]:
 
 
 func _set(property: StringName, value: Variant) -> bool:
-	if property == COMMAND_PROPERTY_NAME:
-		_command_script = _to_command_script(value)
+	if property == _COMMAND_PROPERTY:
+		_command_script = SdtUtils.variant_to_script(value)
 		return true
 	return false
 
 
 func _get(property: StringName) -> Variant:
-	if property == COMMAND_PROPERTY_NAME:
+	if property == _COMMAND_PROPERTY:
 		if _command_script:
 			return _command_script.get_global_name()
 		return &""
-	return null
-
-
-func _to_command_script(value: Variant) -> GDScript:
-	if value == null:
-		return null
-	if value is GDScript:
-		return value
-	if value is String or value is StringName:
-		var script_name: StringName = str(value)
-		if script_name.is_empty():
-			return null
-		return _resolve_script_by_name(script_name)
-	return null
-
-
-func _collect_command_class_names() -> PackedStringArray:
-	return SdtUtils.get_class_list_of_type(Command)
-
-
-func _resolve_script_by_name(script_name: StringName) -> GDScript:
-	var global_classes := ProjectSettings.get_global_class_list()
-	for entry: Dictionary[StringName, StringName] in global_classes:
-		if entry["class"] == script_name:
-			return load(entry["path"]) as GDScript
 	return null
