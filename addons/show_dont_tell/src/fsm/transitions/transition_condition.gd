1252cea32c0f3f4f04ce01a20c8448b54b8eff71
Zack Polson <zack@polson.dev>
2026-08-15 23:18:51 -0600

    human refactor/cleanup of transition condition
    Signed-off-by: Zack Polson <zack@polson.dev>

--- addons/show_dont_tell/src/fsm/transitions/transition_condition.gd
+++ addons/show_dont_tell/src/fsm/transitions/transition_condition.gd
@@ -15,9 +15,6 @@ const _PREFIX_INVERTED := "IfNot_"
 
 func _ready() -> void:
 	_update_name()
-	if Engine.is_editor_hint():
-		return
-
 	_set_condition(condition)
 	if condition:
 		condition.ready()
@@ -35,8 +32,8 @@ func _update_name() -> void:
 			else _PREFIX
 		)
 		if condition:
-			var friendly_name: String = condition._get_friendly_name()
-			if friendly_name != "":
+			var friendly_name := condition._get_friendly_name()
+			if not friendly_name.is_empty():
 				friendly_name = friendly_name.to_pascal_case()
 				name = (prefix + friendly_name).validate_node_name()
 			else:
@@ -46,9 +43,18 @@ func _update_name() -> void:
 
 
 func can_transition() -> bool:
-	if condition:
-		return condition.tick_transition()
-	push_warning("TransitionCondition: No condition assigned for %s" % name)
+	if condition and condition.tick_transition():
+		return true
+	if not condition:
+		push_warning("TransitionCondition: No condition assigned for %s" % name)
+	return _can_children_transition()
+
+
+func _can_children_transition() -> bool:
+	for child: Node in get_children():
+		var child_condition := child as TransitionCondition
+		if child_condition and child_condition.can_transition():
+			return true
 	return false
 
 
@@ -57,51 +63,59 @@ func handle_command(command: Command) -> bool:
 		return false
 	if condition and condition.handle_command(command):
 		return true
+	return _propagate_handle_command(command)
+
+
+func _propagate_handle_command(command: Command) -> bool:
 	for child in get_children():
-		if child is TransitionCondition:
-			var child_condition := child as TransitionCondition
-			if child_condition.handle_command(command):
+		var child_condition := child as TransitionCondition
+		if child_condition:
+			if child_condition._propagate_handle_command(command):
 				return true
 	return false
 
 
 func get_exit_node() -> FiniteState:
-	for child in get_children():
-		if child is TransitionExit:
-			var exit := child as TransitionExit
-			return exit.exit_node
+	for node: Node in get_children():
+		if node is TransitionExit:
+			return (node as TransitionExit).exit_node
 
-		if child is TransitionCondition:
-			var child_condition := child as TransitionCondition
-			if child_condition.tick_transition():
-				if print_exit_transition:
-					print(
-						"Transition condition: ",
-						child_condition.name,
-						", exit node: ",
-						child_condition.get_exit_node()
-					)
-				return child_condition.get_exit_node()
+		var transition := node as TransitionCondition
+		if transition:
+			var exit_node := transition._get_child_exit_node(transition)
+			if exit_node:
+				return exit_node
 
 	push_warning("TransitionCondition: No exit node found for %s" % name)
 	return null
 
 
+func _get_child_exit_node(node: TransitionCondition) -> FiniteState:
+	var child_condition := node as TransitionCondition
+	# ! tick_transition could introduce side effects.
+	if child_condition.tick_transition():
+		var exit_node := child_condition._get_child_exit_node(node)
+		if print_exit_transition:
+			print(
+				"Transition condition: ",
+				child_condition.name,
+				", exit node: ",
+				exit_node.name if exit_node else &"null"
+			)
+		return exit_node
+	return null
+
+
 func _set_condition(new_condition: TransitionOnCondition) -> void:
 	condition = new_condition
-	if condition and not Engine.is_editor_hint():
+	if condition:
 		condition.register_parent(self)
 	_update_name()
-	if (
-		condition
-		and not condition.changed.is_connected(_update_name)
-		and not Engine.is_editor_hint()
-	):
+	if condition and not condition.changed.is_connected(_update_name):
 		condition.changed.connect(_update_name)
 
 
 func _get_configuration_warnings() -> PackedStringArray:
-	var warnings: PackedStringArray = []
 	if not condition:
-		warnings.append("Condition is not assigned.")
-	return warnings
+		return ["Condition is not assigned."]
+	return []
