3b83f103ac4f44c64bc01efc827b144bfa129c75
Zack Polson <zack@polson.dev>
2026-08-16 04:36:11 -0600

    clean up base state unused redundant functions
    Signed-off-by: Zack Polson <zack@polson.dev>

--- addons/show_dont_tell/src/core/base_state.gd
+++ addons/show_dont_tell/src/core/base_state.gd
@@ -8,25 +8,7 @@ extends Node
 # This script is intended to be used as the base for both Behavior Tree and
 # the State Machine. Including the state machines and states themselves.
 
-## Emitted when the [BaseState] has been entered/started.
-signal started
-## Emitted when the [BaseState] has been exited/stopped.
-signal ended
-
 static var debug_get_child_enabled: bool = false
-
-## If set to `true`, the processing will be automatically toggled off or on
-## when the state is changed. It will automatically toggle [method _process]
-## and [method _physics_process] if they are overwritten and enabled
-## in the node file. By default, both are `false`.
-var process_on_active: bool = false:
-	set = set_process_on_active
-
-
-func set_process_on_active(is_enabled: bool) -> void:
-	process_on_active = is_enabled
-	_setup_physics_signal(process_on_active)
-	_setup_process_signal(process_on_active)
 
 
 func get_child_state(state_type: GDScript, internal: bool = false) -> BaseState:
@@ -60,41 +42,12 @@ func _get_child_state_find(state_type: GDScript, _internal: bool) -> Node:
 
 ## Processed when the [BaseState] has been entered.
 func _entered_state() -> void:
-	started.emit()
+	pass
 
 
 ## Processed when the [BaseState] has exited.
 func _exited_state() -> void:
-	ended.emit()
-
-
-func _setup_process_signal(is_enabled: bool) -> void:
-	# TODO: Change this, since it will always have _process.
-	if has_method(&"_process"):
-		if is_enabled:
-			if not started.is_connected(set_process.bind(true)):
-				started.connect(set_process.bind(true))
-			if not ended.is_connected(set_process.bind(false)):
-				ended.connect(set_process.bind(false))
-		else:
-			if started.is_connected(set_process.bind(true)):
-				started.disconnect(set_process.bind(true))
-			if ended.is_connected(set_process.bind(false)):
-				ended.disconnect(set_process.bind(false))
-
-
-func _setup_physics_signal(is_enabled: bool) -> void:
-	if has_method(&"_physics_process"):
-		if is_enabled:
-			if not started.is_connected(set_physics_process.bind(true)):
-				started.connect(set_physics_process.bind(true))
-			if not ended.is_connected(set_physics_process.bind(false)):
-				ended.connect(set_physics_process.bind(false))
-		else:
-			if started.is_connected(set_physics_process.bind(true)):
-				started.disconnect(set_physics_process.bind(true))
-			if ended.is_connected(set_physics_process.bind(false)):
-				ended.disconnect(set_physics_process.bind(false))
+	pass
 
 
 ## For debug purposes. Calls all the search child functions and ensures the
